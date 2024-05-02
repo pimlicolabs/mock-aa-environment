034e53ed2778deb812d0056104cf51c6734f71f9
mous <97399882+mouseless-eth@users.noreply.github.com>
2024-05-02 21:01:25 +0300

    support mainnet forking

--- mock-verifying-paymaster/src/helpers/utils.ts
+++ mock-verifying-paymaster/src/helpers/utils.ts
@@ -1,41 +1,69 @@
-import type { UserOperation } from "permissionless"
-import { http, createWalletClient } from "viem"
-import { mnemonicToAccount } from "viem/accounts"
-import { foundry } from "viem/chains"
-import type { UserOperationV6, UserOperationV7 } from "./schema"
+import type { UserOperation } from "permissionless";
+import {
+	http,
+	createWalletClient,
+	createPublicClient,
+	defineChain,
+} from "viem";
+import { mnemonicToAccount } from "viem/accounts";
+import type { UserOperationV6, UserOperationV7 } from "./schema";
 
 // Type predicate to check if the UserOperation is V07.
 export function isVersion07(
-    operation: UserOperationV6 | UserOperationV7
+	operation: UserOperationV6 | UserOperationV7,
 ): operation is UserOperation<"v0.7"> {
-    return (
-        "factory" in operation ||
-        "paymaster" in operation ||
-        "factoryData" in operation
-    )
+	return (
+		"factory" in operation ||
+		"paymaster" in operation ||
+		"factoryData" in operation
+	);
 }
 
 // Type predicate check if the UserOperation is V06.
 export function isVersion06(
-    operation: UserOperationV6 | UserOperationV7
+	operation: UserOperationV6 | UserOperationV7,
 ): operation is UserOperation<"v0.6"> {
-    return "initCode" in operation || "paymasterAndData" in operation
+	return "initCode" in operation || "paymasterAndData" in operation;
 }
 
-export const getAnvilWalletClient = () => {
-    const account = mnemonicToAccount(
-        "test test test test test test test test test test test junk",
-        {
-            /* avoid nonce error with index 0 when deploying ep contracts. */
-            addressIndex: 1
-        }
-    )
+export const getChain = async () => {
+	const tempClient = createPublicClient({
+		transport: http(process.env.ANVIL_RPC),
+	});
 
-    const walletClient = createWalletClient({
-        account,
-        chain: foundry,
-        transport: http(process.env.ANVIL_RPC)
-    })
+	const chain = defineChain({
+		id: await tempClient.getChainId(),
+		name: "chain",
+		nativeCurrency: {
+			name: "ETH",
+			symbol: "ETH",
+			decimals: 18,
+		},
+		rpcUrls: {
+			default: {
+				http: [],
+				webSocket: undefined,
+			},
+		},
+	});
 
-    return walletClient
-}
+	return chain;
+};
+
+export const getAnvilWalletClient = async () => {
+	const account = mnemonicToAccount(
+		"test test test test test test test test test test test junk",
+		{
+			/* avoid nonce error with index 0 when deploying ep contracts. */
+			addressIndex: 1,
+		},
+	);
+
+	const walletClient = createWalletClient({
+		account,
+		transport: http(process.env.ANVIL_RPC),
+		chain: await getChain(),
+	});
+
+	return walletClient;
+};

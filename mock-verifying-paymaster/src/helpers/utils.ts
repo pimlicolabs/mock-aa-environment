44fd3ae9a5d35671b1f4eca3a94ac21c8e3d75b8
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-06-27 21:16:14 +0100

    add erc-7677 endpoints to mock verifying paymaster

--- mock-verifying-paymaster/src/helpers/utils.ts
+++ mock-verifying-paymaster/src/helpers/utils.ts
@@ -1,21 +1,26 @@
-import { http, createWalletClient } from "viem"
-import { mnemonicToAccount } from "viem/accounts"
-import { foundry } from "viem/chains"
+import { http, createWalletClient } from "viem";
+import { mnemonicToAccount } from "viem/accounts";
+import { foundry } from "viem/chains";
+
+/// Returns the bigger of two BigInts.
+export const maxBigInt = (a: bigint, b: bigint) => {
+	return a > b ? a : b;
+};
 
 export const getAnvilWalletClient = () => {
-    const account = mnemonicToAccount(
-        "test test test test test test test test test test test junk",
-        {
-            /* avoid nonce error with index 0 when deploying ep contracts. */
-            addressIndex: 1
-        }
-    )
+	const account = mnemonicToAccount(
+		"test test test test test test test test test test test junk",
+		{
+			/* avoid nonce error with index 0 when deploying ep contracts. */
+			addressIndex: 1,
+		},
+	);
 
-    const walletClient = createWalletClient({
-        account,
-        chain: foundry,
-        transport: http(process.env.ANVIL_RPC)
-    })
+	const walletClient = createWalletClient({
+		account,
+		chain: foundry,
+		transport: http(process.env.ANVIL_RPC),
+	});
 
-    return walletClient
-}
+	return walletClient;
+};
